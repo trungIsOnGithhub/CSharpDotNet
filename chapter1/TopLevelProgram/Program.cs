aaa714f27cbe18c189765009e92a74938b9a3838
trung <trung@gmail.com>
2023-09-07 15:10:58 +0700

    initial code

---
+++ chapter1/TopLevelProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+/**
+* O(LogN) power method
+**/
+double pow(double x, int n)
+{
+    if(x == 0.0)
+    {
+        return 0.0;
+    }
+
+    if(n == 0 || x == 1.0)
+    {
+        return 1.0;
+    }
+    
+    if(n % 2 == 0)
+    {
+        var temp = pow(x, n/2);
+        return temp * temp;
+    }
+
+    return x * pow(x, n-1);
+}
+
+Console.WriteLine("Test Top-Level Program(only after C# 9):");
+
+Console.WriteLine($"We are working in {Environment.CurrentDirectory}");
+
+for(var d=0.0; d<10.0; d+=1.0)
+{
+    Console.WriteLine($"power of {d} to itself(converted and rounded): {pow( d, Convert.ToInt32(Math.Round(d, MidpointRounding.ToZero)) )}");
+}
---
+++ chapter1/UsualProgram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HelloCS
+{
+    class Program
+    {
+        static double pow(double x, int n)
+        {
+            if(x == 0.0)
+            {
+                return 0.0;
+            }
+
+            if(n == 0 || x == 1.0)
+            {
+                return 1.0;
+            }
+            
+            if(n % 2 == 0)
+            {
+                var temp = pow(x, n/2);
+                return temp * temp;
+            }
+
+            return x * pow(x, n-1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Usual Program(before C#9):");
+
+            Console.WriteLine($"We are working in {Environment.CurrentDirectory}");
+
+            for(var d=0.0; d<10.0; d+=1.0)
+            {
+                Console.WriteLine($"power of {d} to itself(converted and rounded): {pow( d, Convert.ToInt32(Math.Round(d, MidpointRounding.ToZero)) )}");
+            }
+        }
+    }
+}
