4b01899cc3832e197f967350cf94b3ea2fedcabe
Trung Nguyen <trung150502@gmail.com>
2023-10-14 20:38:34 +0700

    Add files via upload

---
+++ chapter8/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Numerics;
+
+uint kindasmall = 1;
+Console.WriteLine($"{kindasmall}");
+
+ulong big = ulong.MaxValue;
+Console.WriteLine($"{big,40:N0}");
+
+BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+Console.WriteLine($"{bigger,40:N0}");
